86cfc04eed1ec689e7153ead72af021be4cae6cc
uts58 <27500463+uts58@users.noreply.github.com>
2024-08-02 19:10:57 -0500

    update

--- qu_j_GSE211395/gcn_impl_signle_chr_no_x.py
+++ qu_j_GSE211395/gcn_impl_signle_chr_no_x.py
@@ -16,52 +16,47 @@ LEARNING_RATE = 0.001
 
 
 chrom_ = [
-    'chr19', 'chrX'
+    'chr1',
+    'chr2', 'chr3', 'chr4', 'chr5',
+    'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
+    'chr11', 'chr12', 'chr13', 'chr14', 'chr15',
+    'chr16', 'chr17', 'chr18', 'chr19',
+    # 'chr20', 'chr21', 'chr22',  #mouse doesn't have these
+    'chrX'
 ]
-
 graph_dir = "/mmfs1/scratch/utsha.saha/mouse_data/data/not_using/qu_j_GSE211395_serum_2i/graphs"
 
 
-def run_thread(no, chrom):
-    for ch in chrom:
-        dir_ = f"{graph_dir}/_{ch}/"
-        print(f'Working on {dir_}, {datetime.datetime.now()}, cuda:{no}')
-        graph_list = list(load_graph_data(dir_).values())
+for ch in chrom:
+    dir_ = f"{graph_dir}/_{ch}/"
+    print(f'Working on {dir_}, {datetime.datetime.now()}, cuda:{no}')
+    graph_list = list(load_graph_data(dir_).values())
 
-        num_nodes = graph_list[0].num_nodes
+    num_nodes = graph_list[0].num_nodes
 
-        device = torch.device(f'cuda:{no}' if torch.cuda.is_available() else 'cpu')
-        model = ModelDeepNoFeatures(num_nodes=num_nodes, embedding_size=EMBEDDING_SIZE, hidden_layers=HIDDEN_LAYERS)
-        model.to(device)
-        optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    device = torch.device(f'cuda:{no}' if torch.cuda.is_available() else 'cpu')
+    model = ModelDeepNoFeatures(num_nodes=num_nodes, embedding_size=EMBEDDING_SIZE, hidden_layers=HIDDEN_LAYERS)
+    model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-        for epoch in range(NUM_EPOCHS):
-            random.shuffle(graph_list)
+    for epoch in range(NUM_EPOCHS):
+        random.shuffle(graph_list)
 
-            total_loss = 0
-            for graph in graph_list:
-                graph_data = graph.to(device)
-                model.train()
-                optimizer.zero_grad()
-                embeddings = model.forward(graph_data)
-                loss = torch.var(embeddings)
-                total_loss += loss.item()
-                loss.backward()
-                optimizer.step()
-                del graph_data
+        total_loss = 0
+        for graph in graph_list:
+            graph_data = graph.to(device)
+            model.train()
+            optimizer.zero_grad()
+            embeddings = model.forward(graph_data)
+            loss = torch.var(embeddings)
+            total_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+            del graph_data
 
-            print(f'{no}: Parent Epoch {epoch}, Average Loss: {total_loss / len(graph_list)}')
-            if epoch % 100 == 0 and epoch != 0:
-                torch.save(model, f'{config_["parent_dir"]}/models/{ch}_no_features_{epoch}.pt')
-                print(f'{no}: {ch} {epoch} saved')
+        print(f'{no}: Parent Epoch {epoch}, Average Loss: {total_loss / len(graph_list)}')
+        if epoch % 100 == 0 and epoch != 0:
+            torch.save(model, f'{config_["parent_dir"]}/models/{ch}_no_features_{epoch}.pt')
+            print(f'{no}: {ch} {epoch} saved')
 
 
-thread_list = []
-for i, items in enumerate(chrom_):
-    thread_list.append(threading.Thread(target=run_thread, args=(i, [items])))
-
-for thread in thread_list:
-    thread.start()
-
-for thread in thread_list:
-    thread.join()
--- qu_j_GSE211395/gcn_impl_single_chr.py
+++ qu_j_GSE211395/gcn_impl_single_chr.py
@@ -17,12 +17,12 @@ graph_dir = f'/mmfs1/scratch/utsha.saha/mouse_data/data/not_using/qu_j_GSE211395
 
 chrom_ = [
     'chr1',
-    # 'chr2', 'chr3', 'chr4', 'chr5',
-    # 'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
-    # 'chr11', 'chr12', 'chr13', 'chr14', 'chr15',
-    # 'chr16', 'chr17', 'chr18', 'chr19',
-    # # 'chr20', 'chr21', 'chr22',  #mouse doesn't have these
-    # 'chrX', 'chrY'
+    'chr2', 'chr3', 'chr4', 'chr5',
+    'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
+    'chr11', 'chr12', 'chr13', 'chr14', 'chr15',
+    'chr16', 'chr17', 'chr18', 'chr19',
+    # 'chr20', 'chr21', 'chr22',  #mouse doesn't have these
+    'chrX'
 ]
 
 for ch in chrom_:
